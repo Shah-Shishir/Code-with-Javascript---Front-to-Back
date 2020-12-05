19a14092f7cd5bc1c60d803b091a00c32515080d
Shah Newaj Shishir <shahshishir.09@gmail.com>
2020-12-06 05:39:53 +0600

    fix bugs and add responsive design

--- Digital Clock with Progress Bar/script.js
+++ Digital Clock with Progress Bar/script.js
@@ -1,60 +1,73 @@
-var hour = document.querySelector("#hour");
-var minute = document.querySelector("#minute");
-var second = document.querySelector("#second");
-var timeFormat = document.querySelector(".time-format");
-var abbrebiation = document.querySelector(".abbr");
-var switchEl = document.querySelector(".switch");
-var progressBar = document.querySelector(".progress-bar");
-var flag = false;
-var format = "24h";
+var hour = document.querySelector('#hour');
+var minute = document.querySelector('#minute');
+var second = document.querySelector('#second');
+var timeFormat = document.querySelector('.time-format');
+var abbrebiation = document.querySelector('.abbr');
+var switchEl = document.querySelector('.switch');
+var progressBar = document.querySelector('.progress-bar');
+var format = '24h';
 
-switchEl.addEventListener("click", switchFormat);
+window.addEventListener('load', function() {
+    format = localStorage.getItem('format');
+    timeFormat.textContent = localStorage.getItem('text');
+    if (localStorage.getItem('abbr')) {
+        abbrebiation.style.visibility = 'visible';
+        abbrebiation.innerText = localStorage.getItem('abbr');
+    }
+});
+
+switchEl.addEventListener('click', switchFormat);
 
 function switchFormat() {
     var date = new Date();
     var currentHours = date.getHours();
-    if (timeFormat.textContent === "24 Hours Format") {
-        timeFormat.textContent = "12 Hours Format";
-        abbrebiation.style.visibility = "visible";
+    if (timeFormat.textContent === '24 Hours Format') {
+        timeFormat.textContent = '12 Hours Format';
+        abbrebiation.style.visibility = 'visible';
+        format = '12h';
+        window.localStorage.setItem('format', '12h');
+        window.localStorage.setItem('text', '12 Hours Format');
         if (currentHours > 12) {
             currentHours -= 12;
             hour.textContent = currentHours < 10 ? ('0' + currentHours) : currentHours;
-            abbrebiation.textContent = "PM";
+            abbrebiation.textContent = 'PM';
+            window.localStorage.setItem('abbr', 'PM');
         } else {
-            abbrebiation.textContent = "AM";
-            if (currentHours === 0) {
-                hour.textContent = "12";
-                format = "12h";
-            }
+            abbrebiation.textContent = 'AM';
+            window.localStorage.setItem('abbr', 'AM');
         }
     } else {
-        timeFormat.textContent = "24 Hours Format";
-        abbrebiation.style.visibility = "hidden";
+        timeFormat.textContent = '24 Hours Format';
+        abbrebiation.style.visibility = 'hidden';
+        format = '24h';
+        window.localStorage.setItem('format', '24h');
+        window.localStorage.removeItem('abbr');
         if (currentHours > 12) {
             currentHours -= 12;
             hour.textContent = currentHours < 10 ? ('0' + currentHours) : currentHours;
         } else if (currentHours === 0) {
-            hour.textContent = "12";
-            format = "12h";
+            hour.textContent = '12';
         }
     }
 }
 
 function setDigitalClock() {
     var date = new Date();
-    if (format === "24h") {
-        var currentHours = date.getHours();
-        hour.textContent = currentHours < 10 ? ('0' + currentHours) : currentHours;
+    var currentHours = date.getHours();
+    if (format === '24h') {
+        timeFormat.textContent = '24 Hours Format';
+    } else {
+        timeFormat.textContent = '12 Hours Format';
+        if (currentHours > 12) {
+            currentHours -= 12;
+        }
     }
+    hour.textContent = currentHours < 10 ? ('0' + currentHours) : currentHours;
     var currentMinutes = date.getMinutes();
     minute.textContent = currentMinutes < 10 ? ('0' + currentMinutes) : currentMinutes;
     var currentSeconds = date.getSeconds();
     second.textContent = currentSeconds < 10 ? ('0' + currentSeconds) : currentSeconds;
-    progressBar.style.width = (currentSeconds/60) * 750 + "px";
-    if (!flag) {
-        timeFormat.textContent = "24 Hours Format";
-        flag = true;
-    }
+    progressBar.style.width = (currentSeconds/60) * 100 + '%';
 }
 
 setInterval(setDigitalClock, 1000);
